17b6130fa50cbe0314fcb9beae0c60e99f1f5946
Adem Hoskin <mailofadem@gmail.com>
2024-03-29 12:09:51 -0400

    model test

--- springMidware/middleware/src/test/java/repo/beat/middleware/MiddlewareApplicationTests.java
+++ springMidware/middleware/src/test/java/repo/beat/middleware/MiddlewareApplicationTests.java
@@ -1,13 +1,45 @@
 package repo.beat.middleware;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import repo.beat.middleware.model.BeatZip;
+
 
 @SpringBootTest
 class MiddlewareApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testZipModel() {
+		String s3Link = "hello:make";
+		String zipName = "hello";
+		Long zipSize = 188888888883L;
+		Date dateAdded = new Date();
+		BeatZip beatZip = new BeatZip(zipName, zipSize, s3Link, dateAdded);
+		assertEquals(beatZip.getZipName(), zipName);
+		assertEquals(beatZip.getZipSize(), zipSize);
+		assertEquals(beatZip.getS3Link(), s3Link);
+		assertEquals(beatZip.getDateAdded(), dateAdded);
+
+		beatZip.setZipName("world");
+		beatZip.setZipSize(188888888883L);
+		beatZip.setS3Link("world:make");
+		Date tempDate = new Date();
+		beatZip.setDateAdded(tempDate);
+		assertEquals(beatZip.getZipName(), "world");
+		assertEquals(beatZip.getZipSize(), 188888888883L);
+		assertEquals(beatZip.getS3Link(), "world:make");
+		assertEquals(beatZip.getDateAdded(), tempDate);
+
 	}
 
 }
