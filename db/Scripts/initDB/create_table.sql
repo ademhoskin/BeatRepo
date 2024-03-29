dfe20e7af7efd9e5ec9cb7c0167a5e6feb330bc4
Adem Hoskin <mailofadem@gmail.com>
2024-03-29 10:44:31 -0400

    fixed app pro in spring

--- db/Scripts/initDB/create_table.sql
+++ db/Scripts/initDB/create_table.sql
@@ -2,6 +2,6 @@ create table if not exists BeatZip (
   zipId serial primary key not null,
   zipName varchar(255) not null,
   zipSize bigint not null,
-  S3Link text not null,
+  s3Link text not null,
   dateAdded date not null
 );
